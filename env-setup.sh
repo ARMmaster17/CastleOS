431460fe9b8b3e80ffa1a939908bb5f2fe2e5499
Joshua Zenn <wordman05@gmail.com>
2021-04-19 23:06:55 -0400

    More environment setup

--- env-setup.sh
+++ env-setup.sh
@@ -6,9 +6,20 @@ git clone git://gcc.gnu.org/git/gcc.git
 cd gcc
 git checkout releases/gcc-9.3.0
 cd ..
+# TODO: Make build tree in separate directory
 git clone git://sourceware.org/git/binutils-gdb.git
 cd binutils-gdb
 git checkout binutils-2_36_1
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
+configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+# TODO: Make build tree in separate directory
+cd ../gcc
+configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
